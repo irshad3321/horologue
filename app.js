6af8b643fe8a19e20717d0bdae0083a7275134b8
irshad3321 <irshadch3321@gmail.com>
2026-07-04 22:49:35 +0530

    prod bug

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ const createSessionConfig = (sessionName, cookieName) => {
       ttl: 7 * 24 * 60 * 60
     }),
     cookie: {
-      secure: process.env.NODE_ENV === 'production',
+      secure: process.env.USE_HTTPS === 'true',
       httpOnly: true,
       maxAge: 1000 * 60 * 60 * 24 * 7,
       sameSite: 'lax'
